c0c24e12edece589172fa1efa0d4686b8a7f0a44
Abhiraj5007 <abhirajgupta5007@gmail.com>
2025-04-02 19:11:47 +0530

    Inital commit

--- views/about_me.py
+++ views/about_me.py
@@ -11,12 +11,12 @@ def show_contact_form():
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 with col1:
-    st.image("./assets/profile_image.png", width=230)
+    st.image("./assets/IMG.png", width=230)
 
 with col2:
-    st.title("Sven Bosau", anchor=False)
+    st.title("Abhiraj Gupta", anchor=False)
     st.write(
-        "Senior Data Analyst, assisting enterprises by supporting data-driven decision-making."
+        "Student Coordinator @Training and Placement Cell MMMMUT | B.tech EE'26"
     )
     if st.button("✉️ Contact Me"):
         show_contact_form()
